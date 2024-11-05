00387c534aaf29ba7173e5fac37ca69c83d5739f
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-11-05 20:06:05 +0100

    todo feedback

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,26 +1,39 @@
+export const options = { good: 1, neutral: 0, bad: 0 };
+export const total = Array.from(Object.values(options)).reduce((acc, curr) => {
+  return acc + curr;
+});
+const posiitve = ((options.good + options.neutral) / total).toFixed(2) * 100;
+
+console.log(posiitve);
+
 export const Feedback = () => {
-  return (
-    <div className="feedback">
-      <div>
-        <span>Good: </span>
-        <span>0</span>
+  const result =
+    total !== 0 ? (
+      <div className="feedback">
+        <div>
+          <span>Good: </span>
+          <span>{options.good}</span>
+        </div>
+        <div>
+          <span>Neutral: </span>
+          <span>{options.neutral}</span>
+        </div>
+        <div>
+          <span>Bad: </span>
+          <span>{options.bad}</span>
+        </div>
+        <div>
+          <span>Total: </span>
+          <span>{total}</span>
+        </div>
+        <div>
+          <span>Positive: </span>
+          <span>{posiitve}%</span>
+        </div>
       </div>
-      <div>
-        <span>Neutral: </span>
-        <span>0</span>
-      </div>
-      <div>
-        <span>Bad: </span>
-        <span>0</span>
-      </div>
-      <div>
-        <span>Total: </span>
-        <span>0</span>
-      </div>
-      <div>
-        <span>Positive: </span>
-        <span>0</span>
-      </div>
-    </div>
-  );
+    ) : (
+      <p>No feedback yet</p>
+    );
+
+  return result;
 };
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,14 +1,33 @@
-const Button = ({ children, idName }) => {
-  return <button type="button">{children}</button>;
+import { useState } from "react";
+import { options, total } from "./Feedback";
+
+const Button = ({ option, children }) => {
+  let [value, setValue] = useState(0);
+
+  const handleClick = () => {
+    setValue(value + 1);
+  };
+
+  return (
+    <button onClick={handleClick} type="button">
+      {children}
+    </button>
+  );
+};
+
+const Reset = ({ children }) => {
+  if (total > 0) {
+    return <Button>Reset</Button>;
+  }
 };
 
 export const Options = () => {
   return (
     <div className="options">
-      <Button>Good</Button>
+      <Button option="good">Good</Button>
       <Button>Neutral</Button>
       <Button>Bad</Button>
-      <Button>Reset</Button>
+      <Reset />
     </div>
   );
 };
