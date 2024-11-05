d0b3d271988ac91d1fb0b360b4fff445fe07eda2
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-11-05 18:43:10 +0100

    description done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 // import Feedback from "./components/Feedback";
 // import Options from "./components/Options";
 
-// import { Description } from "./components/Description";
+import { Description } from "./components/Description";
 // import { Feedback } from "./components/Feedback";
 
 export const App = () => {
-  <>
-    <div className="description">
-      <h1> Sip Happens Café</h1>
-      <p>
-        Please leave your feedback about our service by selecting one of the
-        options below.
-      </p>
-    </div>
-  </>;
+  return <Description />;
 };
