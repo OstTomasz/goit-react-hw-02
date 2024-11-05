d81083b7f2f583da8b285ebd5829e0f7b3b535bf
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-11-05 18:18:54 +0100

    dfg

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import { Example } from "./components/exapmle/Example";
+// import Feedback from "./components/Feedback";
+// import Options from "./components/Options";
+
+// import { Description } from "./components/Description";
+// import { Feedback } from "./components/Feedback";
 
 export const App = () => {
   <>
-    {<Example />}
-    {}
-    {}
+    <div className="description">
+      <h1> Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+    </div>
   </>;
 };
---
+++ src/components/Feedback.jsx
@@ -0,0 +1,12 @@
+export const Feedback = () => {
+  return (
+    <p>sdfgsdfg</p>
+    // <div className="description">
+    //   <h1> Sip Happens Café</h1>
+    //   <p>
+    //     Please leave your feedback about our service by selecting one of the
+    //     options below.
+    //   </p>
+    // </div>
+  );
+};
